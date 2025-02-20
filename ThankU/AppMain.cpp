b99890805dff4742c1ef558dcfe58c91c76c7ef2
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2025-02-20 14:20:28 +0900

    役割別フォルダ作成

--- ThankU/AppMain.cpp
+++ ThankU/AppMain.cpp
@@ -1,10 +1,34 @@
 #include "DxLib.h"
+#include   "Utility/Common.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    unsigned int Cr;
+
+    SetMainWindowText("ThankU");         // ウィンドウタイトルを設定
+
+    ChangeWindowMode(TRUE);                        // ウインドウモードで起動
+
+    SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32); // ウインドウのサイズ
+
+    SetDrawScreen(DX_SCREEN_BACK);                 // 描画先画面を裏にする（ダブルバッファリング）
+
     if (DxLib_Init() == -1)  // ＤＸライブラリ初期化処理
     {
         return -1;           // エラーが起きたら直ちに終了
+    }
+
+    Cr = GetColor(0, 0, 255);        // 青色の値を取得
+    int red, blue, green;
+
+    for (int x = 0; x < 640; x += 20) {
+        for (int y = 0; y < 480; y += 20) {
+            red = GetRand(255);
+            blue = GetRand(255);
+            green = GetRand(255);
+            DrawBoxAA(x, y, x + 18, y + 18, GetColor(red, green, blue), TRUE);    // 四角形を描画
+
+        }
     }
 
     DxLib_End();             // ＤＸライブラリ使用の終了処理
---
+++ ThankU/Utility/Common.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//よく使うor共有すべきマクロはここ
+#define SCREEN_WIDTH 1280		//画面幅(とりあえず1280*720)
+#define SCREEN_HEIGHT 720		//画面高さ
