f9b914928f88d0fbc1202634be9764f54ab97f01
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2025-02-21 10:49:00 +0900

    PadInput:コメント追加

--- ThankU/Utility/PadInput.h
+++ ThankU/Utility/PadInput.h
@@ -6,27 +6,39 @@ class InputControl
 {
 private:
 	static bool now_button[4][16]; //現在のボタン入力
-	static bool old_button[4][16]; //�ߋ��t���[�����͒l
-	static float trigger[4][2];    //���E�g���K�[���͒l
-	static Vector2D stick[4][2];	//���E�X�e�B�b�N���͒l
+	static bool old_button[4][16]; //ひとつ前のボタン入力
+	static float trigger[4][2];    //左右トリガー
+	static Vector2D stick[4][2];	//左右スティック
 
 public:
-	static void Update();	//�X�V����
+	static void Update();	//入力更新
 
-	//�{�^�����͎擾����
-	static bool GetButton(int input_type, int button);	//���������Ă����
-	static bool GetButtonDown(int input_type, int button);	//�������u��
-	static bool GetButtonUp(int input_type, int button);	//�������u��
+	//コントローラー名
+	//DX_INPUT_PAD1　　　　: パッド１
+	//DX_INPUT_PAD2　　　　 : パッド２
+	//DX_INPUT_PAD3　　　　 : パッド３
+	//DX_INPUT_PAD4　　　　 : パッド４
+	//各ボタン取得
+	//押した瞬間 input_type=コントローラー名 button=取得したいボタン
+	static bool GetButton(int input_type, int button);	
+	//押している間 input_type=コントローラー名 button=取得したいボタン
+	static bool GetButtonDown(int input_type, int button);
+	//離した瞬間 input_type=コントローラー名 button=取得したいボタン
+	static bool GetButtonUp(int input_type, int button);	
 
-	//�g���K�[���͎擾����
-	static float GetLeftTrigger(int input_type);			//���g���K�[
-	static float GetRightTrigger(int input_type);			//�E�g���K�[
+	//トリガー
+	//左トリガー取得(input_type=コントローラー名)
+	static float GetLeftTrigger(int input_type);
+	//右トリガー取得(input_type=コントローラー名)
+	static float GetRightTrigger(int input_type);
 
-	//�X�e�B�b�N���͎擾����
-	static Vector2D GetLeftStick(int input_type);			//���X�e�B�b�N
-	static Vector2D GetRightStick(int input_type);		//�E�X�e�B�b�N
+	//スティック取得
+	//左スティック取得(input_type=コントローラー名)
+	static Vector2D GetLeftStick(int input_type);
+	//左スティック取得(input_type=コントローラー名)
+	static Vector2D GetRightStick(int input_type);	
 
 private:
-	//�{�^���z��͈̓`�F�b�N
+	//ボタン範囲測定
 	static bool ChackButtonRange(int button);
 };
