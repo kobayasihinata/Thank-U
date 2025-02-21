a8284c3fd632d0d6d2371c57a04bc58342b28bab
ChinenToshiki <138412696+ChinenToshiki@users.noreply.github.com>
2025-02-21 10:57:03 +0900

    InGame Up

--- ThankU/Scene/InGame/InGameScene.h
+++ ThankU/Scene/InGame/InGameScene.h
@@ -1,1 +1,21 @@
 #pragma once
+#include"DxLib.h"
+#include"../SceneBase.h"
+
+class InGameScene 
+{
+public://プロパティ(データ群)
+
+public:	//メソッド(処理群)
+	InGameScene();			//インストラクター
+	~InGameScene();			//デストラクター
+
+	void Initialize();		//起動時処理
+	void Finalize();		//終了時処理
+	void Update();			//恒常処理
+	void Draw();			//描画処理
+
+
+private:
+
+};
