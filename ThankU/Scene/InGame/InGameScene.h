7e47009dcafbd79c731b3a02d321ae3810378b08
ChinenToshiki <138412696+ChinenToshiki@users.noreply.github.com>
2025-02-21 11:51:57 +0900

    Update InGameScene.h

--- ThankU/Scene/InGame/InGameScene.h
+++ ThankU/Scene/InGame/InGameScene.h
@@ -5,32 +5,20 @@
 class InGameScene  : public SceneBase
 {
 public://�v���p�e�B(�f�[�^�Q)
-	class Player* Player[4];					
+    class Player* Player[4];					
 
 public:	//���\�b�h(�����Q)
-	InGameScene();								//�C���X�g���N�^�[
-	~InGameScene();								//�f�X�g���N�^�[
+    InGameScene();								//
+    ~InGameScene();								//
 
-	virtual void Initialize()	override;		//�N��������
-	virtual void Finalize()		override;		//�I��������
-	virtual eSceneType Update()	override;		//�P�폈��
-	virtual void Draw() const	override;		//�`�揈��
-	class Player* Player[4];																//player
-	class Enemy* Enemy;																		//Enemy
-
-
-public:	//���\�b�h(�����Q)
-	InGameScene();																			//�C���X�g���N�^�[
-	~InGameScene();																			//�f�X�g���N�^�[
-
-	virtual void Initialize()		override;												//�N��������
-	virtual void Finalize()			override;													//�P�폈��
-	virtual void Draw() const		override;											//�I��������
-	virtual eSceneType Update()		override;												//�`�揈��
-
-
-	//���݂̃V�[�������擾
-	virtual eSceneType GetNowScene() const override;
+    virtual void Initialize()	override;		//
+    virtual void Finalize()		override;		//
+    virtual eSceneType Update()	override;		//
+    virtual void Draw() const	override;		//
+    class Player* Player[4];																//player
+    class Enemy* Enemy;																		//Enemy
+    //���݂̃V�[�������擾
+    virtual eSceneType GetNowScene() const override;
 private:
 
 };
