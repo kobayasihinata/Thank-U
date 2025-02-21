cabf1f5781af2abfa0d3aceb07c04a5fbdd29bdb
manato444 <138412544+manato444@users.noreply.github.com>
2025-02-21 11:31:42 +0900

    タイトルシーン各関数の定義
    2025/2/21

---
+++ ThankU/Scene/Title/TitleScene.cpp
@@ -0,0 +1,31 @@
+#include "TitleScene.h"
+
+TitleScene::TitleScene()
+{
+}
+
+TitleScene::~TitleScene()
+{
+}
+
+void TitleScene::Initialize()
+{
+}
+
+void TitleScene::Finalize()
+{
+}
+
+eSceneType TitleScene::Update()
+{
+	return eSceneType();
+}
+
+void TitleScene::Draw() const
+{
+}
+
+eSceneType TitleScene::GetNowScene() const
+{
+	return eSceneType();
+}
