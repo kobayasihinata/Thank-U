3bb02a1008cc6a086ea65c705623aa5a3939f6ef
ChinenToshiki <138412696+ChinenToshiki@users.noreply.github.com>
2025-02-21 11:01:21 +0900

    Update InGameScene.h

--- ThankU/Scene/InGame/InGameScene.h
+++ ThankU/Scene/InGame/InGameScene.h
@@ -2,18 +2,18 @@
 #include"DxLib.h"
 #include"../SceneBase.h"
 
-class InGameScene 
+class InGameScene  : public SceneBase
 {
 public://プロパティ(データ群)
-
+	class Player* Player[4];										//player
 public:	//メソッド(処理群)
-	InGameScene();			//インストラクター
-	~InGameScene();			//デストラクター
+	InGameScene();													//インストラクター
+	~InGameScene();													//デストラクター
 
-	void Initialize();		//起動時処理
-	void Finalize();		//終了時処理
-	void Update();			//恒常処理
-	void Draw();			//描画処理
+	void Initialize();												//起動時処理
+	void Finalize();												//終了時処理
+	void Update();													//恒常処理
+	void Draw();													//描画処理
 
 
 private:
