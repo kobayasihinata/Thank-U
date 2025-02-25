bacf8809afbbf405b297c07a5618063ba4ca98e3
manato444 <138412544+manato444@users.noreply.github.com>
2025-02-25 09:16:13 +0900

    仮

--- ThankU/Scene/Credit/CreditScene.h
+++ ThankU/Scene/Credit/CreditScene.h
@@ -1,27 +1,36 @@
 #pragma once
 #include"DxLib.h"
 #include"../SceneBase.h"
-#include   "../../Utility/Common.h"
+#include "../../Utility/Common.h"
+#include "../../Utility/Effect.h"
 
 class CreditScene : public SceneBase
 {
+private://エフェクト関連
+	//ここにエフェクトのランダム計算とかカウントに使う変数とかを宣言
+
 public://プロパティ(データ群)
-	int count = 3600;	//一定時間経過で終了
-	int scrollY = SCREEN_HEIGHT; // Starting Y position
+
+	int count;	//一定時間経過で終了
+	int scrollY; //スクロール初期位置
+
+public:
+	//エフェクトを使用する際必要
+	Effect effect;
 
 private:
-	int background_image;
-	int credit_logo;
-	int object_image[5];
+	int background_image;	//背景
+	int credit_logo;		//ロゴ
+	int object_image[5];	//各オブジェクト
 
 public:	//メソッド(処理群)
-	CreditScene();													//インストラクター
-	~CreditScene();													//デストラクター
+	CreditScene();			//インストラクター
+	~CreditScene();			//デストラクター
 
-	virtual void Initialize()	override;							//起動時処理
-	virtual void Finalize()		override;							//終了時処理
-	virtual eSceneType Update()	override;							//恒常処理
-	virtual void Draw() const	override;							//描画処理
+	virtual void Initialize()	override;	//起動時処理
+	virtual void Finalize()		override;	//終了時処理
+	virtual eSceneType Update()	override;	//恒常処理
+	virtual void Draw() const	override;	//描画処理
 
 
 	//現在のシーン情報を取得
